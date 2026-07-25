bdaf8344577aa509c82e5435872af1c219e2ce2f
habibu070A <habibusulaiman6778@gmail.com>
2026-07-25 21:28:11 +0100

    Update logo paths for various coins

--- coins.js
+++ coins.js
@@ -16,14 +16,14 @@ export let defaultCoins = [
        symbol: "BNB",
        idGecko: "binancecoin",
        network: "BEP20",
-       logo: "photo/bnb.png",
-       coin_logo: "photo/bnb.png",
+       logo: "bnb.png",
+       coin_logo: "bnb.png",
        rpc: null,
        contract: null
      },
      {
        id: 2,
-       logo: "photo/usdt.png",
+       logo: "usdt.png",
        balance: 0,
        value_ngn: 0,
        price_ngn: 0,
@@ -34,7 +34,7 @@ export let defaultCoins = [
        idGecko: "tether",
        network: "BEP20",
        chainId: 56,
-       coin_logo: "photo/bnb.png",
+       coin_logo: "bnb.png",
        rpc: "https://bsc-datasees.binance.org",
        contract: "0x55d398326f99059fF775485246999027B3197955"
      },
@@ -50,8 +50,8 @@ export let defaultCoins = [
        idGecko: "tether",
        network: "Base",
        chainId: 8453,
-       logo: "photo/usdt.png",
-       coin_logo: "photo/base.png",
+       logo: "usdt.png",
+       coin_logo: "base.png",
        rpc: "https://mainnet.base.org",
        contract: null
      },
@@ -67,8 +67,8 @@ export let defaultCoins = [
        idGecko: "tether",
        network: "Polygon",
        chainId: 137,
-       logo: "photo/usdt.png",
-       coin_logo: "photo/poly.png",
+       logo: "usdt.png",
+       coin_logo: "poly.png",
        rpc: "https://polygon-rpc.com",
        contract: null
      }
