e57f9fbc511e291eec9214ead8e2818df8fea436
habibu070A <habibusulaiman6778@gmail.com>
2026-07-27 07:43:52 +0100

    Delete abi.js

--- abi.js
+++
@@ -1,5 +0,0 @@
-export const ABI = [
-  "function balanceOf(address owner) view returns (uint256)",
-  "function decimal() view returns (uint8)",
-  "function transfer(address to, uint, amount) returns (bool)"
-]
